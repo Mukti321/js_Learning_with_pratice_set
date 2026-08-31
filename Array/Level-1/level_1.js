375bc8d114c7bd2aa24d5cbb9498fbe0231052fc
Mukti  Prasad Dash <muktiprasaddash2@gmail.com>
2026-08-31 22:57:05 +0530

    initial commit

---
+++ Array/Level-1/level_1.js
@@ -0,0 +1,9 @@
+// Array in JavaScript
+//=============================
+//Array is a collection of multiple values stored in a single variable.
+let iplTeams = ["CSK", "MI", "RCB", "KKR", "DC", "SRH", "RR", "PBKS"];
+console.log(iplTeams);
+
+// ex:2 Practice: `colors` naam ka array banao jisme 3 colors ho.
+let colors = ["navy bue", "marron", "white"];
+console.log(colors);
