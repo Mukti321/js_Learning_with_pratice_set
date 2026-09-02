4b0641eca0c1a40656c4e88ba8bae1ca06d6c2e7
Mukti  Prasad Dash <muktiprasaddash2@gmail.com>
2026-09-02 23:21:59 +0530

    Added array code

---
+++ Array/Level-1/level_2.js
@@ -0,0 +1,7 @@
+//push() method
+//----------------
+// The push() method adds one or more elements to the end of an array and returns the new length of the array.
+const fruits = ["apple", "kiwi", "banana", "mango"];
+let newFruits = fruits.push("papaya");
+console.log(newFruits); // push() method not return the array but return the new length of the array.
+console.log(fruits);
